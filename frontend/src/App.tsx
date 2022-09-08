6077421c90272a7bb9569871c8800ba676317634
Rodrigo Sales <rodrigosalesdeo@gmail.com>
2022-09-08 00:55:50 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
+import NotificantionBotton from "./components/NotificantionBotton"
 
 
 function App() {
 
-   
-  return<h1>Olá mundo!</h1>
 
+  return (<>
+    <h1>Olá mundo!</h1>
+    <NotificantionBotton />
+    <NotificantionBotton />
+  </>
+  )
 }
 
 export default App
