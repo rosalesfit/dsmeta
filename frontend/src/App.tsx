3736194dec2878a9d7c6f35ff7471fef658866a0
Rodrigo Sales <rodrigosalesdeo@gmail.com>
2022-09-08 01:57:43 -0300

    other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
-import NotificantionBotton from "./components/NotificantionBotton"
+import Header from "./components/Header"
 
+import SalesCard from "./components/SalesCard"
 
 function App() {
 
 
   return (<>
-    <h1>Olá mundo!</h1>
-    <NotificantionBotton />
-    <NotificantionBotton />
+    <Header /><main>
+      <section id="sales">
+        <div className="dsmeta-container">
+          <SalesCard />
+        </div>
+      </section>
+    </main>
   </>
+
   )
 }
 
